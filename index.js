4008e38929c4f99510b6b217580ec8c231a1b33b
Sam <samuel.waters@gmail.com>
2022-02-24 11:22:26 -0700

    started assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+var customerName = 'Bob';
+return customerName;
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase;
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
